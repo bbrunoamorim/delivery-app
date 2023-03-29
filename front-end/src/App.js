a3b70d344d1a6d259bc17855c17882fd878e8416
luiz henrique <luizhenriquemendes123123@gmail.com>
2023-03-29 20:13:18 -0300

    checkout requisito 17

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" render={ () => <Redirect to="/login" /> } />
       <Route exact path="/login" component={ Login } />
+      <Route exact path="/customer/checkout" component={ Checkout } />
     </Switch>
   );
 }
