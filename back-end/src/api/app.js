78749689e8164eace5dd1bc482e6083a121b7712
iamlucasgomes <lucas.devjs@gmail.com>
2023-03-29 23:20:53 -0300

    Fix: fix in backend app.js file

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -8,13 +8,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-<<<<<<< HEAD
-app.get('/coffee', (_req, res) => res.status(418).end());
-=======
 app.use(express.static('public'));
->>>>>>> 8cf5dbc63b211212b8aa8d7606e0d974c442f194
+
 app.use('/register', RegisterRouter);
 app.use('/login', LoginRouter);
 app.use('/products', ProductRouter);
+app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
