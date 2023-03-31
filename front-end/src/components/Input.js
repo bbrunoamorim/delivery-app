67ccaa8d852ec9c9b045c651fcd8a34507df47e4
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2023-03-31 04:55:28 -0300

    fix: all functions and value input of customer/products

--- front-end/src/components/Input.js
+++ front-end/src/components/Input.js
@@ -2,7 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Input(props) {
-  const { type, className, value, onChange, placeholder, testId } = props;
+  const {
+    type,
+    className,
+    value,
+    onChange,
+    placeholder,
+    testId,
+    id,
+    onKeyDown,
+  } = props;
   return (
     <input
       type={ type }
@@ -11,17 +20,21 @@ function Input(props) {
       onChange={ onChange }
       placeholder={ placeholder }
       data-testid={ testId }
+      id={ id }
+      onKeyDown={ onKeyDown }
     />
   );
 }
 
 Input.propTypes = {
+  id: PropTypes.number.isRequired,
   type: PropTypes.string.isRequired,
   className: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   testId: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
+  onKeyDown: PropTypes.func.isRequired,
 };
 
 export default Input;
--- front-end/src/context/Provider.js
+++ front-end/src/context/Provider.js
@@ -1,16 +1,24 @@
-import React, { useState, useMemo } from 'react';
-import propTypes from 'prop-types';
-import AppContext from './Context';
+import React, { useState, useMemo } from "react";
+import propTypes from "prop-types";
+import AppContext from "./Context";
 
 export default function Provider({ children }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [btnLogin, setBtnLogin] = useState(true);
   const [error, setError] = useState(false);
   const [products, setProducts] = useState([]);
-  const [quantityProducts, setQuantityProducts] = useState(0);
+  const [quantityProducts, setQuantityProducts] = useState({});
   const [disableQuantity, setDisableQuantity] = useState(true);
   const [valorTotal, setValorTotal] = useState(0);
+  const [inputValue, setInputValue] = useState({});
+
+  const handleInputValue = (id, value) => {
+    setInputValue((prevstate) => ({
+      ...prevstate,
+      [id]: value,
+    }));
+  };
 
   const context = useMemo(
     () => ({
@@ -30,10 +38,23 @@ export default function Provider({ children }) {
       setDisableQuantity,
       valorTotal,
       setValorTotal,
+      inputValue,
+      setInputValue,
+      handleInputValue,
     }),
-    [email, password, btnLogin, error, products, quantityProducts, disableQuantity, valorTotal],
+    [
+      email,
+      password,
+      btnLogin,
+      error,
+      products,
+      quantityProducts,
+      disableQuantity,
+      valorTotal,
+      inputValue,
+    ]
   );
-  return <AppContext.Provider value={ context }>{children}</AppContext.Provider>;
+  return <AppContext.Provider value={context}>{children}</AppContext.Provider>;
 }
 
 Provider.propTypes = {
